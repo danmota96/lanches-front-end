9e04435852d4ed79150ba47b96444a6399efba93
Daniel Mota <97922515+danmota96@users.noreply.github.com>
2022-04-18 18:27:18 -0300

    feat:  Leitura de paletas individuais (por ID)

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ async function findAllLanches() {
   const response = await fetch(`${baseUrl}/lista-lanches`);
 
   const lanches = await response.json();
-  
+
   lanches.forEach((lanche) => {
     document.getElementById("LancheList").insertAdjacentHTML(
       "beforeend",
@@ -12,7 +12,9 @@ async function findAllLanches() {
       <div class="ListaLancheItem">
         <div class="ListaLancheItem__local">${lanche.local}</div>
         <div class="ListaLancheItem__localizacao">${lanche.localizacao}</div>
-        <img src=${lanche.foto} alt=${lanche.nome} class="ListaLancheItem__foto" width="25%">
+        <img src=${lanche.foto} alt=${
+        lanche.nome
+    } class="ListaLancheItem__foto" width="25%">
         <div class="ListaLancheItem__nome">${lanche.nome}</div>
         <div class="ListaLancheItem__descricao">${lanche.descricao}</div>
         <div class="ListaLancheItem__preco">${lanche.preco.toFixed(2)}</div>
@@ -20,6 +22,31 @@ async function findAllLanches() {
     </div>  `
     );
   });
-};
+}
+
+//localizar um lanche pelo seu ID
+
+async function findByIdLanches(){
+  const id = document.querySelector("#idLanche").value
+  const response = await fetch(`${baseUrl}/lista-lanches/${id}`);
+  const lanche = await response.json();
+
+  const lancheEscolhidoDiv = document.querySelector("#lancheEscolhido");
+
+  lancheEscolhidoDiv.innerHTML = `
+  <div class="LancheCardItem" id="LancheCardItem_${lanche.id}">
+  <div>
+  <div class="LancheCardItem">
+  <div class="LancheCardItem__local">${lanche.local}</div>
+  <div class="LancheCardItem__localizacao">${lanche.localizacao}</div>
+  <img src=${lanche.foto} alt=${
+    lanche.nome
+  } class="LancheCardItem__foto" width="25%">
+  <div class="LancheCardItem__nome">${lanche.nome}</div>
+  <div class="LancheCardItem__descricao">${lanche.descricao}</div>
+  <div class="LancheCardItem__preco">${lanche.preco.toFixed(2)}</div>
+</div> 
+</div>`;
+}
 
 findAllLanches();
