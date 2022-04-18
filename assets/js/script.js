3229112e5b159bc725307e134731f45b5da6abd8
Daniel Mota <97922515+danmota96@users.noreply.github.com>
2022-04-18 17:22:09 -0300

    feat: rota dinâmica

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,24 @@
 const baseUrl = "http://localhost:3000/lanches";
 
-//Localizar todos os lanches do backend
 async function findAllLanches() {
   const response = await fetch(`${baseUrl}/lista-lanches`);
-  const lanches = await response.json();
 
-  lanches.forEach((lanches) => {
-  document.getElementById('LancheLista').insertAdjacentHTML(
-    //primeiro parâmetro
-    'beforeend',
-    //segundo parâmetro a ser adicionado no HTML
-    `
-    <div class="LancheLista">
-    <div class="ListaLancheItem">
-      <div class="ListaLancheItem__local">${lanche.local}</div>
-      <div class="ListaLancheItem__localizacao">${lanche.localizacao}</div>
-      <img src=${lanche.foto} alt=${lanche.nome} class="ListaLancheItem__foto" width="25%">
-      <div class="ListaLancheItem__nome">${lanche.nome}</div>
-      <div class="ListaLancheItem__descricao">${lanche.descricao}</div>
-      <div class="ListaLancheItem__preco">${lanche.preco.toFixed(2)}</div>
-    </div>
-  </div>  
-    `,
-  );
+  const lanches = await response.json();
+  
+  lanches.forEach((lanche) => {
+    document.getElementById("LancheList").insertAdjacentHTML(
+      "beforeend",
+      `<div class="LancheLista">
+      <div class="ListaLancheItem">
+        <div class="ListaLancheItem__local">${lanche.local}</div>
+        <div class="ListaLancheItem__localizacao">${lanche.localizacao}</div>
+        <img src=${lanche.foto} alt=${lanche.nome} class="ListaLancheItem__foto" width="25%">
+        <div class="ListaLancheItem__nome">${lanche.nome}</div>
+        <div class="ListaLancheItem__descricao">${lanche.descricao}</div>
+        <div class="ListaLancheItem__preco">${lanche.preco.toFixed(2)}</div>
+      </div>
+    </div>  `
+    );
   });
 };
 
