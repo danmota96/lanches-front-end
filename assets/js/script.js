ca1f0891ba3d50fafb2196d3bdf9f21f2959bc85
Daniel Mota <97922515+danmota96@users.noreply.github.com>
2022-04-19 21:02:25 -0300

    feat: adicionando novos arquivos para CSS

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,25 +8,26 @@ async function findAllLanches() {
   lanches.forEach((lanche) => {
     document.getElementById("LancheList").insertAdjacentHTML(
       "beforeend",
-      `  <div class="LancheLista" id="LancheList">
-      <div class="ListaLancheItem">
+      `<div class="LancheLista" id="LancheList">
         <div class="ListaLancheItem__local">${lanche.local}</div>
         <div class="ListaLancheItem__localizacao">${lanche.localizacao}</div>
       
         <div class="ListaLancheItem__nome">${lanche.nome}</div>
         <div class="ListaLancheItem__descricao">${lanche.descricao}</div>
         <div class="ListaLancheItem__preco">${lanche.preco.toFixed(2)}</div>
+        <div class="LancheListaItem__acoes Acoes">
+          <button class="Acoes__editar btn" onclick="abrirModal(${
+            lanche.id
+          })">Editar</button> 
+        <button class="Acoes__apagar btn" onclick="abrirModalDelete(${
+          lanche.id
+        })">Apagar</button> 
       </div>
-    </div> 
-    <div class="LancheListaItem__acoes Acoes">
-    <button class="Acoes__editar btn" onclick="abrirModal(${
-      lanche.id
-    })">Editar</button> 
-    <button class="Acoes__apagar btn">Apagar</button> 
-    </div>
-    <img src=${lanche.foto} alt=${
+      </div>
+      <img src=${lanche.foto} alt=${
         lanche.nome
-      } class="LancheCardItem__foto" width="25%">`
+      } class="LancheCardItem__foto" >
+  </div> `
     );
   });
 }
@@ -42,16 +43,25 @@ async function findByIdLanches() {
 
   lancheEscolhidoDiv.innerHTML = `
   <div class="LancheCardItem" id="LancheCardItem_${lanche.id}">
-  <div>
-  <div class="LancheCardItem">
-  <div class="LancheCardItem__local">${lanche.local}</div>
-  <div class="LancheCardItem__localizacao">${lanche.localizacao}</div>
-  <div class="LancheCardItem__nome">${lanche.nome}</div>
-  <div class="LancheCardItem__descricao">${lanche.descricao}</div>
-  <div class="LancheCardItem__preco">${lanche.preco.toFixed(2)}</div>
-</div> 
-</div>
-<img src=${lanche.foto} alt=${
+      <div>
+      <div class="LancheCardItem">
+      <div class="LancheCardItem__local">${lanche.local}</div>
+      <div class="LancheCardItem__localizacao">${lanche.localizacao}</div>
+      <div class="LancheCardItem__nome">${lanche.nome}</div>
+      <div class="LancheCardItem__descricao">${lanche.descricao}</div>
+      <div class="LancheCardItem__preco">${lanche.preco.toFixed(2)}</div>
+      </div> 
+      </div>
+      <div class="LancheListaItem__acoes Acoes">
+              <button class="Acoes__editar btn" onclick="abrirModal(${
+                lanche.id
+              })">Editar</button> 
+            <button class="Acoes__apagar btn" onclick="abrirModalDelete(${
+              lanche.id
+            })">Apagar</button> 
+      </div>
+  </div>
+  <img src=${lanche.foto} alt=${
     lanche.nome
   } class="LancheCardItem__foto" width="25%">`;
 }
@@ -59,21 +69,42 @@ async function findByIdLanches() {
 findAllLanches();
 
 //MODAL PARA CADASTRO
-function abrirModalCadastro() {
+async function abrirModal(id = null) {
+  if (id != null) {
+    document.querySelector("#title-header-modal").innerText =
+      "Atualizar um Lanche";
+    document.querySelector("#button-form-modal").innerText = "Atualizar";
+
+    const response = await fetch(`${baseURL}/lanche/${id}`);
+    const lanche = await response.json();
+
+    document.querySelector("#local").value = lanche.local;
+    document.querySelector("#localizacao").value = lanche.localizacao;
+    document.querySelector("#descricao").value = lanche.descricao;
+    document.querySelector("#nome").value = lanche.nome;
+    document.querySelector("#preco").value = lanche.preco;
+    document.querySelector("#foto").value = lanche.foto;
+    document.querySelector("#id").value = lanche.id;
+  } else {
+    document.querySelector("#title-header-modal").innerText =
+      "Cadastrar um Lanche";
+    document.querySelector("#button-form-modal").innerText = "Cadastrar";
+  }
+
   document.querySelector(".modal-overlay").style.display = "flex";
 }
 
-function fecharModalCadastro() {
+function fecharModal() {
   document.querySelector(".modal-overlay").style.display = "none";
   document.querySelector("#local").value = "";
   document.querySelector("#localizacao").value = "";
-  document.querySelector("#foto").value = "";
   document.querySelector("#nome").value = "";
   document.querySelector("#descricao").value = "";
   document.querySelector("#preco").value = 0;
 }
 
 async function createLanche() {
+  const id = document.getElementById("id").value;
   const local = document.querySelector("#local").value;
   const localizacao = document.querySelector("#localizacao").value;
   const foto = document.querySelector("#foto").value;
@@ -82,6 +113,7 @@ async function createLanche() {
   const preco = document.querySelector("#preco").value;
 
   const lanche = {
+    id,
     local,
     localizacao,
     foto,
@@ -89,39 +121,78 @@ async function createLanche() {
     descricao,
     preco,
   };
+  const modoEdicaoAtivado = id > 0;
 
-  const response = await fetch(baseUrl + "/create", {
-    method: "post",
+  const endpoint = baseUrl + (modoEdicaoAtivado ? `/update/${id}` : "/create");
+
+  const response = await fetch(endpoint, {
+    method: modoEdicaoAtivado ? "put" : "post",
     headers: {
       "Content-Type": "application/json",
     },
     mode: "cors",
-    body: JSON.stringify(paleta),
+    body: JSON.stringify(lanche),
   });
 
   const novoLanche = await response.json();
 
   const html = `
-<div class="LancheCardItem" id="LancheListaItem_{$novoLanche.id}">
-<div>
-<div class="LancheCardItem__local">${novoLanche.local}</div>
-<div class="LancheCardItem__localizacao">${novoLanche.localizacao}</div>
-<div class="LancheCardItem__nome">${novoLanche.nome}</div>
-<div class="LancheCardItem__descricao">${novoLanche.descricao}</div>
-<div class="LancheCardItem__preco">${novoLanche.preco.toFixed(2)}</div>
-</div> 
-</div>
-<div class="LancheListaItem__acoes Acoes">
-    <button class="Acoes__editar btn" onclick="abrirModal(${
-      lanche.id
-    })">Editar</button> 
-    <button class="Acoes__apagar btn">Apagar</button> 
-</div>
-<img src=${novoLanche.foto} alt=${
+      <div class="LancheCardItem" id="LancheListaItem_{$novoLanche.id}">
+      <div>
+      <div class="LancheCardItem__local">${novoLanche.local}</div>
+      <div class="LancheCardItem__localizacao">${novoLanche.localizacao}</div>
+      <div class="LancheCardItem__nome">${novoLanche.nome}</div>
+      <div class="LancheCardItem__descricao">${novoLanche.descricao}</div>
+      <div class="LancheCardItem__preco">${novoLanche.preco.toFixed(2)}</div>
+      </div> 
+      </div>
+      <div class="LancheListaItem__acoes Acoes">
+          <button class="Acoes__editar btn" onclick="abrirModal(${
+            lanche.id
+          })">Editar</button> 
+        <button class="Acoes__apagar btn" onclick="abrirModalDelete(${
+          lanche.id
+        })">Apagar</button> 
+      </div>
+      <img src=${novoLanche.foto} alt=${
     novoLanche.nome
   } class="LancheCardItem__foto" width="25%">`;
 
-  document.getElementById("LancheList").insertAdjacentHTML("beforeend", html);
+  if (modoEdicaoAtivado) {
+    document.querySelector(`#LancheListaItem_${id}`).outerHTML = html;
+  } else {
+    document.querySelector("#LancheList").insertAdjacentHTML("beforeend", html);
+  }
 
-  fecharModalCadastro();
+  fecharModal();
 }
+
+function abrirModalDelete(id) {
+  document.querySelector("#overlay-delete").style.display = "flex";
+
+  const btnSim = document.querySelector(".btn_delete_yes");
+
+  btnSim.addEventListener("click", function () {
+    deleteLanche(id);
+  });
+}
+
+function fecharModalDelete() {
+  document.querySelector("#overlay-delete").style.display = "none";
+}
+
+const deleteLanche = async (id) => {
+  const response = await fetch(`${baseUrl}/delete/${id}`, {
+    method: "delete",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    mode: "cors",
+  });
+  const result = await response.json();
+  alert(result.message);
+  document.getElementById("lancheList").innerHTML = "";
+
+  findAllLanches();
+  fecharModalDelete();
+};
