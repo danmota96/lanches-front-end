90bfe2b237831cc20c14295e8bc09ea0e26b973f
Daniel Mota <97922515+danmota96@users.noreply.github.com>
2022-04-30 19:39:44 -0300

    feat: navbar

---
+++ cardapio-lanches-2022/src/components/NavBar/Navbar.jsx
@@ -0,0 +1,33 @@
+import "./Navbar.css";
+import sacola from "assets/icons/sacola.svg";
+import logo from "assets/logo.svg";
+
+function Navbar() {
+  return (
+    <div className="Header">
+      <div className="row">
+        <div className="Header__logo Logo">
+          <img
+            src={logo}
+            width="250px"
+            alt="Logo LFAV"
+            className="Logo__icone"
+          />
+          <span className="Logo__titulo"> Lanches Favoritos </span>
+        </div>
+        <div className="Header__opcoes Opcoes">
+          <div className="Opcoes__sacola Sacola">
+            <img
+              src={sacola}
+              width="40px"
+              className="Sacola__icone"
+              alt="Sacola de compras"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
